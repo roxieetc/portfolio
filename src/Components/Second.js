0a971f3e5e09126fc7d108cae38a87d17fdd1559
roxieetc <roxieschnoor@yahoo.com>
2022-01-26 15:23:44 -0800

    changes to about me section

--- src/Components/Second.js
+++ src/Components/Second.js
@@ -13,8 +13,7 @@ const Second = () => {
 
                 <div className="text-2">
                     <p>Hi, my name is Roxie Schnoor and I am a software engineer. My passion to create and excitement to learn
-                    have helped me to become a competent and confident developer who is looking to push the boundaries of innovation
-                    and design.</p>
+                    have helped me to become a competent and confident developer that I am today.</p>
                     <br/>
                     <p>From learning frontend languages on my own, such as: HTML5, CSS3, and JavaScript, to expanding my knowledge
                        and skillset through the General Assembly Software Engineering Bootcamp, I have continued to grow my passion
