932b755219dda1d36e909a7b3305499b54c937ff
roxieetc <roxieschnoor@yahoo.com>
2022-01-29 16:35:43 -0800

    Added tech stack

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -30,7 +30,8 @@ const Projects = () => {
                     <a href="https://github.com/roxieetc/clique-frontend" target="_blank" rel="noreferrer"><button className='projectLink'>GitHub</button></a></div>
                     <h5>A photo-sharing social media app created with ReactJS, Python, Django, and PostgreSQL, 
                         This app allows users to sign up and share photos with their friends. Comments, accounts, 
-                        and posts allow for full CRUD so that users can make sure they're sharing what they want to share.</h5>
+                        and posts allow for full CRUD so that users can make sure they're sharing what they want to share.
+                        Features a mock design of Instagram.</h5>
                         <h6>ReactJS, Python, Django, PostgreSQL, Bootstrap</h6>
                 </div>
 
@@ -52,10 +53,10 @@ const Projects = () => {
                     <div className='buttons'>
                     <a href="https://roxieetc.github.io/FFX-Trivia/" target="_blank" rel="noreferrer"><button className='projectLink'>Live Site</button></a>
                     <a href="https://github.com/roxieetc/FFX-Trivia" target="_blank" rel="noreferrer"><button className='projectLink'>GitHub</button></a></div>
-                    <h5>Recipe generator app created using ReactJS and themealdb API. This app allows users to look up recipes by category. 
-                        Each recipe features an image of the dish, instructions on how to prepare, and a YouTube link for visual preparation. 
-                        Users can also utilize the random recipe generator.</h5>
-                        <h6>HTML, CSS, JavaScript</h6>
+                    <h5>A fun and interactive trivia game that tests your knowledge of this fan-favorite franchise RPG. 
+                        Work your way through a 10-question character quiz that will prove or not. Your final score will 
+                        be displayed at the end with question indicators that will let you know if the answer you chose was wrong or right.</h5>
+                        <h6>HTML5, CSS3, JavaScript DOM</h6>
                 </div>
 
             </div>
--- src/Components/Second.js
+++ src/Components/Second.js
@@ -26,6 +26,23 @@ const Second = () => {
                     </p>
                 </div>
             </div>
+
+            <div className="container-2">
+                <div className="text-2">
+                    <i class="devicon-html5-plain-wordmark colored"></i>
+                    <i class="devicon-css3-plain-wordmark colored"></i>
+                    <i class="devicon-javascript-plain colored"></i>
+                    <i class="devicon-react-original-wordmark colored"></i>
+                    <i class="devicon-bootstrap-plain-wordmark"></i>
+                    <i class="devicon-nodejs-plain-wordmark colored"></i>
+                    <i class="devicon-mongodb-plain-wordmark colored"></i>
+                    <i class="devicon-python-plain-wordmark colored"></i>
+                    <i class="devicon-django-plain"></i>
+                    <i class="devicon-postgresql-plain-wordmark colored"></i>
+                    <i class="devicon-canva-original"></i>
+                    <i class="devicon-vscode-plain colored"></i>
+                    </div>
+                </div>
         </section>
     )
 }
