be969654241be86c28648bd4ab392ee0b88fdef3
roxieetc <roxieschnoor@yahoo.com>
2022-01-31 18:12:18 -0800

    added bullet points

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -20,7 +20,7 @@ function Projects() {
                     <h5>Created using ReactJS and the unofficial Valorant API, this app showcases all in-game information related to Riot Games' Valorant. 
                     Featuring data regarding agents, weapons, weapon skins, and other player information, this app shows off a responsive design similar to 
                     what a player would find in the game.</h5>
-                    <h6>ReactJS . Unofficial Valorant API . Responsive Design</h6>
+                    <h6>ReactJS • Unofficial Valorant API • Responsive Design</h6>
                     </div>
 
                 <div className="cart">
@@ -32,7 +32,7 @@ function Projects() {
                         This app allows users to sign up and share photos with their friends. Comments, accounts, 
                         and posts allow for full CRUD so that users can make sure they're sharing what they want to share.
                         Features a mock design of Instagram.</h5>
-                        <h6>ReactJS . Python . Django . PostgreSQL . Bootstrap</h6>
+                        <h6>ReactJS • Python • Django • PostgreSQL • Bootstrap</h6>
                 </div>
 
                 <div className="cart">
@@ -44,7 +44,7 @@ function Projects() {
                     <h5>Recipe generator app created using ReactJS and themealdb API. This app allows users to look up recipes by category. 
                         Each recipe features an image of the dish, instructions on how to prepare, and a YouTube link for visual preparation. 
                         Users can also utilize the random recipe generator.</h5>
-                        <h6>ReactJS . themealdb API</h6>
+                        <h6>ReactJS • themealdb API</h6>
                 </div>
 
                 <div className="cart">
@@ -56,7 +56,7 @@ function Projects() {
                     <h5>A fun and interactive trivia game that tests your knowledge of this longtime fan-favorite franchise RPG. 
                         Work your way through a 10-question character quiz that will calculate a final score at the end. 
                         Each button will flash red or green on selection to indicate right and wrong answers for each question.</h5>
-                        <h6>HTML5 . CSS3 . JavaScript DOM</h6>
+                        <h6>HTML5 • CSS3 • JavaScript DOM</h6>
                 </div>
 
             </div>
